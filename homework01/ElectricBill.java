c1ca62eec93c32d4dc1bc69efb02df0efb8af22f
rad2025 <richardarbury@delta.edu>
2025-01-16 23:57:42 -0500

    Update ElectricBill.java

--- homework01/ElectricBill.java
+++ homework01/ElectricBill.java
@@ -18,10 +18,45 @@ public class ElectricBill
    // Method calcElecBill
    // This method ...
    
-   public static double calcElecBill(int month, int energy)
-   {
-      return 0;
-   }
+   public static double calcElecBill(int monthcode, int kWh) {
+        //Constants for billing charges as specified
+        final double BASE_CHARGE = 8.34;
+        final double WINTER_RATE_LOW = 0.223;
+        final double WINTER_RATE_HIGH = 0.315;
+        final int WINTER_THRESHOLD = 800;
+        final double NON_WINTER_RATE_LOW = 0.184;
+        final double NON_WINTER_RATE_HIGH = 0.293;
+        final int NON_WINTER_THRESHOLD = 700;
+
+        double usageCharge = 0.0;
+
+        // using boolean logic to determine the month
+        boolean isWinter = (monthcode == 12 || monthcode == 1 || monthcode == 2);
+
+        if (isWinter) {
+            // for winter billing calculation
+            if (kWh <= WINTER_THRESHOLD) {
+                usageCharge = kWh * WINTER_RATE_LOW;
+            } else {
+                usageCharge = (WINTER_THRESHOLD * WINTER_RATE_LOW) +
+                        ((kWh - WINTER_THRESHOLD) * WINTER_RATE_HIGH);
+            }
+        } else {
+            // Non-winter billing calculation
+            if (kWh <= NON_WINTER_THRESHOLD) {
+                usageCharge = kWh * NON_WINTER_RATE_LOW;
+            } else {
+                usageCharge = (NON_WINTER_THRESHOLD * NON_WINTER_RATE_LOW) +
+                        ((kWh - NON_WINTER_THRESHOLD) * NON_WINTER_RATE_HIGH);
+            }
+        }
+
+        // Add the base charge
+        double totalcharge = usageCharge + BASE_CHARGE;
+
+        // Round to two decimal places and return
+        return Math.round(totalcharge * 100.0) / 100.0;
+    }
    
 }
 
