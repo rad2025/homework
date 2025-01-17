ca814d43b97f0e102d5952edec9f58a2fe2c7cf1
rad2025 <richardarbury@delta.edu>
2025-01-16 23:54:59 -0500

    Update DrawSlash.java

--- homework01/DrawSlash.java
+++ homework01/DrawSlash.java
@@ -22,6 +22,35 @@ public class DrawSlash
    }
    
    // Method drawSlash
+   public static void drawSlash(int size, String direction) {
+        //Validate input parameters
+        if (size < 2 || size > 10 || (!direction.equals("L") && !direction.equals("R"))) {
+            System.out.println("?");
+            return;
+        }
+
+        //Draw left slash, starting from the bottom left and moves upward right
+        if (direction.equals("L")) {
+            for (int i = 0; i < size; i++) {
+                //Print spaces before the star
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
+
+        //Draw right slash, starts from the top-left and moves downward right
+        else if (direction.equals("R")) {
+            for (int i = 0; i < size; i++) {
+                //Print spaces before the star
+                for (int j = 0; j < size - i - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
+    }
    
 
 
