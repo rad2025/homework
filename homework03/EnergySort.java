11c406eee807baf6f80851285a7e7078aef1a14e
rad2025 <richardarbury@delta.edu>
2025-01-30 21:16:59 -0500

    Update EnergySort.java

--- homework03/EnergySort.java
+++ homework03/EnergySort.java
@@ -73,14 +73,37 @@ public class EnergySort
 
     // Sort arrays ascending chronologically
     public static void sortChronologically(int month[], int year[], int energy[])
-    {
+    { //here we use i to ensure multiple passes while j compares adjacent elements
+        for (int i = 0; i < NUMBER_MONTHS - 1; i++) {
+            for (int j = 0; j < NUMBER_MONTHS - 1 - i; j++) {
+                if (year[j] > year[j + 1] || (year[j] == year[j + 1] && month[j] > month[j + 1])) {
+                    swap(month, j, j + 1); //if current year is greater, swap, same for months on current years
+                    swap(year, j, j + 1); //oh and we use the swap fucntion to actually swap (defined below)
+                    swap(energy, j, j + 1);
+                }
+            }
+        }
 
     }
 
     // Sort arrays descending by energy usage
     public static void sortByDescendingEnergy(int month[], int year[], int energy[])
     {
-
+        for (int i = 0; i < NUMBER_MONTHS - 1; i++) {
+            for (int j = 0; j < NUMBER_MONTHS - 1 - i; j++) { //same logic as before but just for energy
+                if (energy[j] < energy[j + 1]) {
+                    swap(month, j, j + 1); //again using the swap helper
+                    swap(year, j, j + 1);
+                    swap(energy, j, j + 1);
+                }
+            }
+        }
+    }
+    //swap helper function, similar to what was used in an earlier part of the homework
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i]; //makes a temp for the current node
+        arr[i] = arr[j]; //swaps nodes
+        arr[j] = temp; //sets latter mode to the temp
     }
 }
  
